7b97e663c492e04497e2e2e9e65e86a914dd1576
Dan Michell <dan.michell1000@gmail.com>
2022-05-13 12:23:59 +0100

    Added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <footer class="p-4 bg-slate-500 rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 w-screen snap-center">
-      <span class="text-lg text-gray-300 sm:text-center">Dan Michell</span>
+      <span class="text-lg text-gray-300 sm:text-center">Thank you for viewing!</span>
       <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-300 sm:mt-0">
         <li>
           <a href="#" class="mr-4 hover:underline md:mr-6 ">
