3a4f400ba76fa54acc6e10ab34759fb8382c1e8d
Dan Michell <dan.michell1000@gmail.com>
2022-06-13 14:18:54 +0100

    Added work

--- src/components/Work.js
+++ src/components/Work.js
@@ -4,6 +4,7 @@ import NutribudProj from "./Projects/NutribudProj";
 import SocialNetworkProj from "./Projects/SocialNetworkProj";
 import WorldBankProj from "./Projects/WorldBankProj";
 import GameOfLifeProj from "./Projects/GameOfLifeProj";
+import LinkShortenerProj from "./Projects/LinkShortenderProj";
 
 function Work() {
   return (
@@ -14,7 +15,6 @@ function Work() {
             <h1 className="font-medium text-8xl mb-3">My Work.</h1>
             <h3 className="font-light text-gray-400 text-4xl">Personal and professional projects.</h3>
           </div>
-
           <div className="flex justify-center items-center gap-5 mt-10">
             <h1 className="text-4xl">Take a look...</h1>
             <MdOutlineSwipe size={86} className="text-gray-300 animate-swipe origin-bottom" />
@@ -31,6 +31,9 @@ function Work() {
         </div>
         <div class="carousel-item w-full">
           <GameOfLifeProj />
+        </div>
+        <div class="carousel-item w-full">
+          <LinkShortenerProj />
         </div>
       </div>
       <button className="flex items-center justify-center mt-auto mb-3 hover:scale-110 transition-all">
