7d71cf8ad005e0d222449b3fe30ce159b4709127
Dan Michell <dan.michell1000@gmail.com>
2022-07-02 13:51:00 +0100

    About me optimised

--- src/components/AboutPages/AboutMe.js
+++ src/components/AboutPages/AboutMe.js
@@ -1,19 +1,21 @@
 function AboutMe() {
   return (
-    <div className="carousel-item w-full flex flex-col justify-center" id="about-me">
-      <div className="ml-[10%] text-gray-200">
-        <h1 className="font-medium text-7xl mb-3">About me.</h1>
-        <h3 className="font-light text-gray-400 text-3xl">A bit about my background and experience.</h3>
+    <div className="carousel-item w-full flex flex-col justify-start sm:justify-center" id="about-me">
+      <div className="sm:ml-[10%] text-gray-200 flex flex-col items-center">
+        <h1 className="font-medium text-6xl sm:text-7xl mb-1 mt-3 sm:mb-3 sm:mt-0">About me.</h1>
+        <h3 className="font-light text-gray-400 text-xl w-[80%] text-center sm:text-left sm:text-3xl sm:w-auto">
+          A bit about my background and experience.
+        </h3>
       </div>
-      <div className="w-[85%] flex justify-center items-center self-center mt-[6vh]">
-        <div className="flex w-[25%] min-w-[150px] max-w-[300px] justify-center items-center mx-16">
+      <div className="sm:w-[85%] flex flex-col justify-center items-center self-center sm:mt-[6vh] sm:flex-row">
+        <div className="flex sm:w-[25%] sm:min-w-[150px] sm:max-w-[300px] max-w-[200px] justify-center items-center mx-16  my-5 sm:my-0">
           <img
             className="rounded-full"
             alt="Dan Michell"
             src="https://drive.google.com/uc?export=view&id=1wucOFHiV3wLZX-bM4HAz_4fbqjOL4SPJ"
           />
         </div>
-        <div className="flex items-center w-[60%]">
+        <div className="flex items-center sm:w-[60%] w-[90%]">
           <p className="text-xl font-light leading-relaxed text-gray-300">
             Thank you for taking the time to look at my website! I'm Dan, a junior fullstack software engineer based
             near London. Having recently entered the world of Software Development from a background in Mechanical
