3e04502bd3ca2ff62e0f31dbf4e65927309c1335
Dan Michell <dan.michell1000@gmail.com>
2022-07-02 13:59:33 +0100

    Skills optimised

--- src/components/AboutPages/Skills.js
+++ src/components/AboutPages/Skills.js
@@ -13,19 +13,21 @@ import {
 
 function Skills() {
   return (
-    <div className="carousel-item w-full flex flex-col items-center justify-center" id="skills">
-      <div className="ml-[15%] text-gray-200 w-full">
-        <h1 className="font-medium text-7xl mb-3">Skills.</h1>
-        <h3 className="font-light text-gray-400 text-3xl">Technologies I have been working with recently.</h3>
+    <div className="carousel-item w-full flex flex-col items-center justify-start sm:justify-center" id="skills">
+      <div className="sm:ml-[15%] text-gray-200 w-full flex flex-col items-center sm:items-start mt-3 sm:mt-0">
+        <h1 className="font-medium text-6xl sm:text-7xl mb-3">Skills.</h1>
+        <h3 className="font-light text-gray-400 text-xl sm:text-3xl text-center sm:text-left">
+          Technologies I have been working with recently.
+        </h3>
       </div>
-      <div className="w-[80%] flex justify-center flex-wrap gap-20 mt-[6vh]">
+      <div className="sm:w-[80%] w-[90%] flex justify-center flex-wrap sm:gap-20 gap-10 sm:mt-[6vh] mt-3">
         <div className="flex flex-col items-center justify-center">
           <button className="flex items-center justify-center hover:scale-110 transition-all">
             <a href="https://reactjs.org/" target="_blank" rel="noreferrer">
               <SiReact size={94} className="text-gray-300" />
             </a>
           </button>
-          <p className="mt-1 text-lg ">React / React Native</p>
+          <p className="mt-1 text-lg ">React / Native</p>
         </div>
         <div className="flex flex-col items-center justify-center">
           <button className="flex items-center justify-center hover:scale-110 transition-all">
