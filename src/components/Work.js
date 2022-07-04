105872bdb57600f7a884acf01f050b2024901296
Dan Michell <dan.michell1000@gmail.com>
2022-07-04 11:51:21 +0100

    Work page optimised

--- src/components/Work.js
+++ src/components/Work.js
@@ -14,13 +14,13 @@ function Work() {
     <div className="flex flex-col items-center h-full">
       <div class="w-full h-[100vh] carousel rounded-box bg-opacity-20 text-gray-300">
         <div class="carousel-item w-full flex flex-col items-center justify-center gap-10">
-          <div>
-            <h1 className="font-medium text-8xl mb-3">My Work.</h1>
-            <h3 className="font-light text-gray-400 text-4xl">Personal and professional projects.</h3>
+          <div className="flex flex-col justify-center items-center">
+            <h1 className="font-medium sm:text-9xl text-7xl mb-3">My Work.</h1>
+            <h3 className="font-light text-gray-400 sm:text-4xl text-2xl">Personal and professional projects.</h3>
           </div>
-          <div className="flex justify-center items-center gap-5 mt-10">
-            <h1 className="text-4xl">Take a look...</h1>
-            <MdOutlineSwipe size={86} className="text-gray-300 animate-swipe origin-bottom" />
+          <div className="flex justify-center items-center gap-5 mt-10 sm:flex-row flex-col">
+            <h1 className="sm:text-4xl text-3xl">Take a look...</h1>
+            <MdOutlineSwipe size={76} className="text-gray-300 animate-swipe origin-bottom" />
           </div>
         </div>
         {populateProjects()}
