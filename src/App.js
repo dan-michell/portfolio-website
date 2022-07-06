9fb8116170a6e5f42c66196441a3599f8963b051
Dan Michell <dan.michell1000@gmail.com>
2022-07-06 11:14:18 +0100

    Changed header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { useState, useEffect, useRef } from "react";
-import HeaderTwo from "./components/HeaderTwo";
+import HeaderThree from "./components/HeaderThree";
 import LandingContent from "./components/LandingContent";
 import Footer from "./components/Footer";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Work from "./components/Work";
-import TOPOLOGY from "vanta/src/vanta.topology";
+import NET from "vanta/src/vanta.net";
+
 function App() {
   const [vantaEffect, setVantaEffect] = useState(0);
   const myRef = useRef(null);
   useEffect(() => {
     if (!vantaEffect) {
       setVantaEffect(
-        TOPOLOGY({
+        NET({
           el: myRef.current,
           backgroundColor: 0x010718,
           color: 0x4f5564,
@@ -25,10 +26,12 @@ function App() {
   }, [vantaEffect]);
 
   return (
-    <div ref={myRef} className="bg-[#010718] ">
-      <div className="h-screen flex items-center flex-col justify-start" id="home">
-        <HeaderTwo />
-        <LandingContent />
+    <div className="bg-[#010718]">
+      <div ref={myRef}>
+        <div className="h-screen flex items-center flex-col justify-start" id="home">
+          <HeaderThree />
+          <LandingContent />
+        </div>
       </div>
       <About />
       <Work />
---
+++ src/components/HeaderThree.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { BsThreeDots } from "react-icons/bs";
+import { GoTerminal } from "react-icons/go";
+
+export default function HeaderThree() {
+  return (
+    <div className="navbar px-10">
+      <div className="flex-1">
+        <div className="flex items-center">
+          <GoTerminal size={40} className="text-gray-300" />
+          <span className="text-xl text-gray-300 ml-3 font-mono">Dan Michell</span>
+        </div>
+      </div>
+      <div className="dropdown dropdown-end">
+        <label tabindex="0" className="btn btn-ghost btn-circle avatar">
+          <BsThreeDots size={24} />
+        </label>
+        <ul tabindex="0" className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
+          <li>
+            <a href="#about" className="justify-between">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#work">Work</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
